1efabe2f82b33df56bd8d8404ed399acabd3d9a8
Erisnake <162056736+Erisnake@users.noreply.github.com>
2024-03-14 16:35:32 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <>
       <div className="flex items-center space-x-5 flex-col lg:flex-row">
-        <img src="https://static.vecteezy.com/ti/vettori-gratis/p3/615673-medico-croce-e-salute-farmacia-logo-modello-vettoriale.jpg" className="w-40 ml-5" alt="" />
+        <img src="../logo.jpg" className="w-40 ml-5" alt="" />
         <h1 className="font-black text-5xl text-center sm:mb-5 md:w-2/3 mx-auto">
           Seguimiento Pacientes {""}
           <span className="text-pink-700">Clínica</span>
